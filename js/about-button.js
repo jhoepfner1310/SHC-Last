94fba5f9e3451bae6bb293528f5518d9688bccf6
jhoepfner <jhoepfner@stud.hs-heilbronn.de>
2025-09-17 15:59:27 +0200

    interaktive optimiert

--- js/about-button.js
+++ js/about-button.js
@@ -2,12 +2,14 @@ const aboutButton = document.getElementById("about-us-button");
 const aboutContent = document.getElementById("aboutUsContent");
 const aboutUsTitle = document.getElementById("aboutUsTitle");
 
-let buttonClicked = false;
+let buttonClicked = true;
 
 function toggleContent() {
     if (!buttonClicked) {
         aboutContent.style.display = "none";
         aboutUsTitle.style.display = "none";
+        aboutContent.style.opacity = "0";
+        aboutUsTitle.style.opacity = "0";
         buttonClicked = true;
 
         //Klick Animation
@@ -17,6 +19,8 @@ function toggleContent() {
     } else {
         aboutContent.style.display = "block";
         aboutUsTitle.style.display = "block";
+        aboutContent.style.opacity = "1";
+        aboutUsTitle.style.opacity = "1";
         buttonClicked = false;
 
         //Klick Animation
@@ -36,7 +40,7 @@ function stylesAnimation() {
     aboutButton.style.backgroundColor = "var(--grün)";
     aboutButton.style.transform = "scale(1.05)";
 
-    setTimeout(resetStyles ,500);
+    setTimeout(resetStyles ,600);
 }
 
 aboutButton.addEventListener("click", toggleContent);
